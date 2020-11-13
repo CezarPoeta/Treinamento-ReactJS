64e88b7bc542d2cf58bd03cd65b866d4209acb11
Cezar Roberto Poeta <cezarrobertopoeta@gmail.com>
2020-11-13 19:20:19 -0300

    Completed Part One
    Completed Part One

--- src/App.js
+++ src/App.js
@@ -9,19 +9,48 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      title: 'Aplicação de tarefas',
-      ntareas: 10
+      todos: todos
     }
   }
 
   render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p> {todo.description} </p>
+              <p><mark> {todo.responsible} </mark> </p>
+            </div>
+          </div>
+        </div>
+
+
+      )
+    })
+
     return (
       <div className="App">
         <nav className="navbar navbar-dark bg-dark">
           <a href="" className="text-white">
-            {this.state.title} - {this.state.ntareas}
+            Tasks
+            <span className="badge gadge-pill badge-ligth ml-2">
+              {this.state.todos.length}
+            </span>
           </a>
         </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+            {todos}
+          </div>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
       </div>
 
